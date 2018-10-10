744646ded531e41a55a6a21c3c2064af177c7385
Borsuk <Jakub.Borsuk@student.tdsb.on.ca>
2018-10-10 12:51:16 -0400

    Initial commit

---
+++ src/labtester/Computer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labtester;
+
+/**
+ *
+ * @author 342720539
+ */
+public class Computer {
+    
+}
