f226c9704577726727f7565bfbd7be0fc4fb2e84
unknown <Jakub.Borsuk@student.tdsb.on.ca>
2018-10-12 13:13:53 -0400

    woah

--- src/labtester/LabTester.java
+++ src/labtester/LabTester.java
@@ -41,5 +41,29 @@ public class LabTester {
             e.printStackTrace();
         }
 
+        while(true){
+            System.out.println("Computers in the Lab\n");
+            
+            for(int i = 0; i<lab.getComputersInstalled(); i++){
+                System.out.println("Computer: "+(i+1));
+                System.out.println(lab.getComputers().get(i).toString()+"\n");
+            }
+            
+            System.out.println("Choices:");
+            System.out.println("1. Output average age of computers in lab.");
+            System.out.println("2. Output specs of the newest computer.");
+            System.out.println("3. Output specs of the fastest computer, indicate if it has the largest RAM.");
+            System.out.println("4. Output computers with a manufacturer of your choice.");
+            System.out.println("5. Output serial number of computers expiring next year.");
+            System.out.printf("%s%5s","Enter your choice (1-5):","");
+            int choice = input.nextInt();
+            input.nextLine();
+            System.out.println("");
+            
+            if(choice == 1){
+                
+            }
+            break;
+        }
     }
 }
